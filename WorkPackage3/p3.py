0044d6743fd55c9bf5e792aa10d4f1818ba6d655
JamesFalconer <Jamesfalconer007@gmail.com>
2021-09-11 11:01:53 +0200

    Update p3.py

--- WorkPackage3/p3.py
+++ WorkPackage3/p3.py
@@ -5,7 +5,6 @@ import ES2EEPROMUtils
 import os
 import time
 
-# some global variables that need to change as we run the program
 end_of_game = None  # set if the user wins or ends the game
 
 current_guess = 0
@@ -95,7 +94,7 @@ def setup():
     global ledPWM
     buzzerPWM = GPIO.PWM(buzzer, 1)
     ledPWM = GPIO.PWM(LED_accuracy, 50)
-    
+    #set all used pins to low
     GPIO.output(LED_value[0], GPIO.LOW)
     GPIO.output(LED_value[1], GPIO.LOW)
     GPIO.output(LED_value[2], GPIO.LOW)
@@ -160,10 +159,6 @@ def generate_number():
 # Increase button pressed
 def btn_increase_pressed(channel):
     global current_guess
-    # Increase the value shown on the LEDs
-    # You can choose to have a global variable store the user's current guess, 
-    # or just pull the value off the LEDs when a user makes a guess
-
     if current_guess != 7 :
         current_guess+=1
     else:
@@ -190,17 +185,6 @@ def btn_increase_pressed(channel):
 # Guess button
 def btn_guess_pressed(channel):
     global value, current_guess, num_guess
-    # If they've pressed and held the button, clear up the GPIO and take them back to the menu screen
-    # Compare the actual value with the user value displayed on the LEDs
-    # Change the PWM LED
-    # if it's close enough, adjust the buzzer
-    # if it's an exact guess:
-    # - Disable LEDs and Buzzer
-    # - tell the user and prompt them for a name
-    # - fetch all the scores
-    # - add the new score
-    # - sort the scores
-    # - Store the scores back to the EEPROM, being sure to update the score count
     start_time = time.time()
     while GPIO.input(channel) == 0:
         pass
@@ -248,10 +232,6 @@ def btn_guess_pressed(channel):
 # LED Brightness
 def accuracy_leds():
     global first_time, value, current_guess
-    # Set the brightness of the LED based on how close the guess is to the answer
-    # - The % brightness should be directly proportional to the % "closeness"
-    # - For example if the answer is 6 and a user guesses 4, the brightness should be at 4/6*100 = 66%
-    # - If they guessed 7, the brightness would be at ((8-7)/(8-6)*100 = 50%
             
     dutyCycle = 100 - abs(value-current_guess)/7*100
 
@@ -266,12 +246,6 @@ def accuracy_leds():
 
 # Sound Buzzer
 def trigger_buzzer():
-    # The buzzer operates differently from the LED
-    # While we want the brightness of the LED to change(duty cycle), we want the frequency of the buzzer to change
-    # The buzzer duty cycle should be left at 50%
-    # If the user is off by an absolute value of 3, the buzzer should sound once every second
-    # If the user is off by an absolute value of 2, the buzzer should sound twice every second
-    # If the user is off by an absolute value of 1, the buzzer should sound 4 times a second
     if abs(value - current_guess) >= 3:
         buzzerPWM.start(50)
     elif abs(value - current_guess) == 2:
